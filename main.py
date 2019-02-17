dc293f899e30bbf0493a647127e3052c049a1861
Honzung Chang <honzungchang@Honzungs-MacBook-Pro.local>
2019-02-17 13:39:45 +0800

    take command line arguments

--- main.py
+++ main.py
@@ -1,12 +1,63 @@
+import sys
 import json
 import codecs
-from pprint import pprint
 
-ver = "zh_cuv"
+from pprint import pprint
+from getopt import getopt
+from getopt import error
+
+
+def parse_arg_verses(s):
+    ls = s.split(',')
+    verses = []
+    for x in ls:
+        if x.isdigit():
+            verses.append(int(x))
+        else:
+            x = x.split('-')
+            x = [int(n) for n in x if n.isdigit()]
+            if len(x) > 0:
+                for n in range(x[0], x[1] + 1):
+                    verses.append(n)
+    return verses
+
+
+arg_book = 'John'
+arg_chpt = '1'
+arg_verses = '1-7'
+arg_version = "zh_cuv"
+
+arg_list = sys.argv[1:]
+unix_options = "b:c:v:u:"
+gnu_options = ["book=", "chapter=", "verses=", "version="]
+
+try:
+    arguments, values = getopt(arg_list, unix_options, gnu_options)
+except error as err:
+    # output error, and return with an error code
+    print(str(err))
+    sys.exit(2)
+
+for k, v in arguments:
+    if k in ('-b', '--book'):
+        arg_book = v
+    elif k in ('-c', '--chapter'):
+        arg_chpt = v
+    elif k in ('-v', '--verses'):
+        arg_verses = v
+    elif k in ('-u', '--version'):
+        arg_version = v
+
+verse_num_list = parse_arg_verses(arg_verses)
+
+# pprint(verse_num_list)
+
+print("Book %s chapter %s verse(s) %s in version %s" %
+      (arg_book, arg_chpt, arg_verses, arg_version))
 
 src_path = "/Users/honzungchang/GitRepo/bible/json/"
 
-filename = src_path + ver + '.json'
+filename = src_path + arg_version + '.json'
 
 with open('./book_abbrevs.json') as f:
     book_abbrevs = json.load(f)
@@ -15,15 +66,39 @@ with open('./book_names.json') as f:
 
 
 def main():
-    c = codecs.open(filename, 'r', 'utf-8-sig')
+    try:
+        c = codecs.open(filename, 'r', 'utf-8-sig')
+    except:
+        print("Error: no such version '%s'" % arg_version)
+        return
     bible = json.load(c)
 
     # for book in bible:
     #     pprint(book['abbrev'])
 
-    book = bible[book_abbrevs.index('gn')]  # keys: abbrev, chapters, name
+    ind = -1
+    if arg_book in book_abbrevs:
+        ind = book_abbrevs.index(arg_book)
+    if arg_book in book_names:
+        ind = book_names.index(arg_book)
+
+    if ind == -1:
+        print("Error: no such book '%s'" % arg_book)
+        return
+
+    book = bible[ind]  # keys: abbrev, chapters, name
     chapters = book['chapters']
-    pprint(chapters[0][1])
+
+    n_chapter = int(arg_chpt) - 1
+
+    if n_chapter < 0 or n_chapter > len(chapters):
+        print("Error: no such chapter %s" % arg_chpt)
+        return
+
+    verses = chapters[n_chapter]
+    for n in verse_num_list:
+        if n-1 <= len(verses):
+            print("%s\t%s" % (n, verses[n - 1]))
     return
 
 
