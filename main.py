8e8ed0aad37862a72a9a5d8386363ea4f56c1a01
Honzung Chang <honzungchang@Honzungs-MacBook-Pro.local>
2019-02-18 17:18:44 +0800

    update

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import sys
 import json
 import codecs
+import os
 
 from pprint import pprint
 from getopt import getopt
 from getopt import error
+
+# cwd = os.getcwd()
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
 
 def parse_arg_verses(s):
@@ -24,12 +28,11 @@ def parse_arg_verses(s):
 
 arg_book = ''
 arg_chpt = ''
-arg_verses = ''
 arg_version = 'zh_cuv'
 
 arg_list = sys.argv[1:]
-unix_options = "b:c:v:u:"
-gnu_options = ["book=", "chapter=", "verses=", "version="]
+unix_options = "b:c:v:"
+gnu_options = ["book=", "chapter=", "version="]
 
 try:
     arguments, values = getopt(arg_list, unix_options, gnu_options)
@@ -43,81 +46,77 @@ for k, v in arguments:
         arg_book = v
     elif k in ('-c', '--chapter'):
         arg_chpt = v
-    elif k in ('-v', '--verses'):
-        arg_verses = v
-    elif k in ('-u', '--version'):
+    elif k in ('-v', '--version'):
         arg_version = v
 
-verse_num_list = parse_arg_verses(arg_verses)
-
-src_path = "/Users/honzungchang/GitRepo/bible/json/"
-
-filename = src_path + arg_version + '.json'
-
-with open('./book_abbrevs.json') as f:
+with open(dir_path + '/book_abbrevs.json') as f:
     book_abbrevs = json.load(f)
-with open('./book_names.json') as f:
+with open(dir_path + '/book_names.json') as f:
     book_names = json.load(f)
 
 if arg_book == '':
     print(
-        "Usage: bible --book=name [--chapter=1] [--verses=1,2,7,8-20] [--version=zh_cuv]")
+        "Usage: bible --book=name [--chapter=1[:1,2,7,8-20]] [--version=zh_cuv]")
     print("Please select a book:")
     for i, book in enumerate(book_names):
         sys.stdout.write(book + ' (' + book_abbrevs[i] + ')' + ' | ')
     print()
     sys.exit(0)
 
+src_path = dir_path + "/_data"
 
-def main():
-    try:
-        c = codecs.open(filename, 'r', 'utf-8-sig')
-    except:
-        print("Error: no such version '%s'" % arg_version)
-        return
-    bible = json.load(c)
+filename = src_path + '/' + arg_version + '.json'
 
-    # for book in bible:
-    #     pprint(book['abbrev'])
+try:
+    c = codecs.open(filename, 'r', 'utf-8-sig')
+except:
+    print("Error: no such version '%s'" % arg_version)
+    sys.exit(2)
 
-    ind = -1
-    if arg_book in book_abbrevs:
-        ind = book_abbrevs.index(arg_book)
-    if arg_book in book_names:
-        ind = book_names.index(arg_book)
+bible = json.load(c)
 
-    if ind == -1:
-        print("Error: no such book '%s'" % arg_book)
-        return
+ind = -1
+if arg_book in book_abbrevs:
+    ind = book_abbrevs.index(arg_book)
+if arg_book in book_names:
+    ind = book_names.index(arg_book)
 
-    book = bible[ind]  # keys: abbrev, chapters, name
-    chapters = book['chapters']
+if ind == -1:
+    print("Error: no such book '%s'" % arg_book)
+    sys.exit(2)
 
-    if arg_chpt == '':
-        print("Please select a chapter (1 to %s)" % len(chapters))
-        return
+book = bible[ind]  # keys: abbrev, chapters, name
+chapters = book['chapters']
 
-    selected_book_name = book_names[ind]
-    print("Book of %s chapter %s (%s)" %
-          (selected_book_name, arg_chpt, arg_version))
+if arg_chpt == '':
+    print("Please select a chapter (1 to %s)" % len(chapters))
+    sys.exit(0)
 
-    n_chapter = int(arg_chpt) - 1
+verse_num_list = []
 
-    if n_chapter < 0 or n_chapter > len(chapters):
-        print("Error: no such chapter %s" % arg_chpt)
-        return
+chpt = arg_chpt.split(':')
+if len(chpt) > 1:
+    verse_num_list = parse_arg_verses(chpt[1])
 
-    verses = chapters[n_chapter]
-    if len(verse_num_list) == 0:
-        for n, s in enumerate(verses):
-            print("%s\t%s" % (n+1, s))
-    else:
-        for n in verse_num_list:
-            if n-1 >= 0 and n-1 <= len(verses):
-                print("%s\t%s" % (n, verses[n - 1]))
-    return
+selected_book_name = book_names[ind]
+print("Book of %s chapter %s (%s)" %
+      (selected_book_name, chpt[0], arg_version))
+
+n_chapter = int(chpt[0]) - 1
+
+if n_chapter < 0 or n_chapter > len(chapters):
+    print("Error: no such chapter %s" % chpt[0])
+    sys.exit(2)
+
+verses = chapters[n_chapter]
+if len(verse_num_list) == 0:
+    for n, s in enumerate(verses):
+        print("%s\t%s" % (n+1, s))
+else:
+    for n in verse_num_list:
+        if n-1 >= 0 and n-1 <= len(verses):
+            print("%s\t%s" % (n, verses[n - 1]))
 
 
 if __name__ == "__main__":
-    main()
     pass
